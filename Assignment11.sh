086250f23538081c7bcc71899524dc18d97466aa
Abdus Sadik Molla <abdussadikmolla@gmail.com>
2022-11-24 22:04:30 +0530

    Correct Assignment11.sh

--- Assignment11.sh
+++ Assignment11.sh
@@ -1,9 +1,9 @@
 # WAP in shell script to find whether a user is valid or not
-echo enter the user name
-read n
-a=`cut -d ":" -f 1 /etc/passwd | grep $n`
-if [ [ "$a" == "$n"  ]
-then valid user
-else
-echo $a
+echo Enter the user name:
+read user
+if [ `grep -c $user /etc/passwd` -eq 0 ]
+then
+echo Invalid user
+else 
+echo Valid user
 fi
